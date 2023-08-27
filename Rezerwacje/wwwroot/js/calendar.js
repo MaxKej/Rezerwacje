9fa5c341fc0863b75e88723409c9095563704f2b
Maksymilian Kudelski <308123@stud.umk.pl>
2023-08-27 15:08:14 +0200

    Daty
    Wyświetlanie rezerwacji w zależności od daty

--- Rezerwacje/wwwroot/js/calendar.js
+++ Rezerwacje/wwwroot/js/calendar.js
@@ -1,4 +1,9 @@
-﻿async function userData() {
+﻿let userBookings;
+const startDayOfWeek = 0;
+let date_today = new Date();
+let startDate, endDate;
+
+async function userData() {
     try {
         const userIdResponse = await fetch("/api/user/get/userid");
         const usernameResponse = await fetch("/api/user/get/username");
@@ -49,38 +54,50 @@ function displayUserBookings(userBookings, userName) {
     });
 }
 
-function addUserBookings(userBookings) {
-    const dayNames = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"];
+function showBookingForm() {
+    const bookingForm = document.getElementById("bookingForm");
+    if (bookingForm.style.display === "block")
+        bookingForm.style.display = "none";
+    else
+        bookingForm.style.display = "block";
+}
+
+function addUserBookings(userBookings, startOfWeek, endOfWeek) {
+    const dayNames = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
     const columns = document.querySelectorAll(".column");
 
-    userBookings.forEach((booking, index) => {
+
+    userBookings.forEach((booking) => {
         const bookingDate_s = new Date(booking.beginOfBooking);
         const bookingDate_e = new Date(booking.endOfBooking);
-        const dayName = dayNames[bookingDate_s.getDay()];
-        const columnIndex = bookingDate_s.getDay();
 
-        const hours_s = bookingDate_s.getHours();
-        const minutes_s = bookingDate_s.getMinutes().toString().padStart(2, '0');
+        // Check if booking is within the displayed week
+        if (bookingDate_s >= startOfWeek && bookingDate_e <= endOfWeek) {
+            const dayName = dayNames[bookingDate_s.getDay()];
+            const columnIndex = bookingDate_s.getDay();
 
-        const hours_e = bookingDate_e.getHours();
-        const minutes_e = bookingDate_e.getMinutes().toString().padStart(2, '0');
+            const hours_s = bookingDate_s.getHours();
+            const minutes_s = bookingDate_s.getMinutes().toString().padStart(2, '0');
 
-        const bookingDiv = document.createElement("div");
-        bookingDiv.classList.add("booking");
-        bookingDiv.innerHTML = `${hours_s}:${minutes_s} - ${hours_e}:${minutes_e} <br> ${booking.userName}`;
+            const hours_e = bookingDate_e.getHours();
+            const minutes_e = bookingDate_e.getMinutes().toString().padStart(2, '0');
+            const bookingDiv = document.createElement("div");
+            bookingDiv.classList.add("booking");
 
-        const tooltipText = document.createElement("p");
-        tooltipText.classList.add("tooltip-text");
-        tooltipText.textContent = booking.description;
-        bookingDiv.appendChild(tooltipText);
+            let user = booking.userName.split("@");
 
-        if (index === 0) {
-            columns[columnIndex].prepend(bookingDiv);
-        } else {
+            bookingDiv.innerHTML = `${hours_s}:${minutes_s} - ${hours_e}:${minutes_e} <br> ${user[0]}`;
+
+            const tooltipText = document.createElement("p");
+            tooltipText.classList.add("tooltip-text");
+            tooltipText.textContent = booking.description;
+            bookingDiv.appendChild(tooltipText);
+
             columns[columnIndex].appendChild(bookingDiv);
         }
     });
 }
+
 
 async function saveUserBooking() {
     const usernameResponse = await fetch("/api/user/get/username");
@@ -98,6 +115,11 @@ async function saveUserBooking() {
     collision = await Collision(new Date(beginOfBooking), new Date(endOfBooking));
     if (collision) {
         document.getElementById("result").textContent = "Termin niedostępny!";
+        return;
+    }
+
+    if (areDifferentDays(new Date(beginOfBooking), new Date(endOfBooking))) {
+        document.getElementById("result").textContent = "Początek i koniec muszą być tego samego dnia!";
         return;
     }
 
@@ -166,6 +188,30 @@ async function Collision(beginOfBooking, endOfBooking) {
     return false;
 }
 
+function areDifferentDays(startDate, endDate) {
+    return (
+        startDate.getFullYear() !== endDate.getFullYear() ||
+        startDate.getMonth() !== endDate.getMonth() ||
+        startDate.getDate() !== endDate.getDate()
+    );
+}
+
+function loadCurrentWeek(startDate, endDate, date_today) {
+    const newStartDate = new Date(date_today);
+    newStartDate.setDate(date_today.getDate() - date_today.getDay());
+    newStartDate.setHours(0, 0, 0, 0);
+
+    const newEndDate = new Date(date_today);
+    newEndDate.setDate(date_today.getDate() - date_today.getDay() + 6);
+    newEndDate.setHours(23, 59, 59, 999);
+
+    startDate.setTime(newStartDate.getTime());
+    endDate.setTime(newEndDate.getTime());
+
+    const divElement = document.getElementById("daty");
+    divElement.textContent = formatDate(startDate) + " - " + formatDate(endDate);
+}
+
 function displayDate(dateString) {
     const dateObj = new Date(dateString);
 
@@ -191,4 +237,48 @@ function formatDate(date) {
     return `${day}-${month}-${year}`;
 }
 
+function getStartOfWeek(date, startDay) {
+    const day = date.getDay();
+    const diff = (day - startDay + 7) % 7;
+    const startDate = new Date(date);
+    startDate.setDate(date.getDate() - diff);
+    startDate.setHours(0, 0, 0, 0);
+    return startDate;
+}
 
+function updateWeekDates(date, startDay) {
+    startDate = getStartOfWeek(date, startDay);
+    endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + 6);
+    endDate.setHours(23, 59, 59, 999);
+}
+
+function updateDisplayedDates() {
+    const divElement = document.getElementById("daty");
+    divElement.textContent = formatDate(startDate) + " - " + formatDate(endDate);
+}
+
+document.getElementById("arrow_left").addEventListener("click", function () {
+    date_today.setDate(date_today.getDate() - 7);
+    updateWeekDates(date_today, startDayOfWeek);
+    clearCalendar();
+    addUserBookings(userBookings, startDate, endDate);
+    updateDisplayedDates();
+});
+
+document.getElementById("arrow_right").addEventListener("click", function () {
+    date_today.setDate(date_today.getDate() + 7);
+    updateWeekDates(date_today, startDayOfWeek);
+    clearCalendar();
+    addUserBookings(userBookings, startDate, endDate);
+    updateDisplayedDates();
+});
+
+function clearCalendar() {
+    const columns = document.querySelectorAll(".column");
+    columns.forEach(column => {
+        while (column.firstChild) {
+            column.removeChild(column.firstChild);
+        }
+    });
+}
